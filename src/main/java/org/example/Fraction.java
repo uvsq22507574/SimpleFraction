240e571b2c85daffb0f81117e6b03ae6c6295c20
BATOCFETOU Ditorgma Komla Daniel <komlabatocgmail.com>
2025-09-25 20:52:06 +0200

    mise à jour code

--- src/main/java/org/example/Fraction.java
+++ src/main/java/org/example/Fraction.java
@@ -1,5 +1,45 @@
 package org.example;
 
 public class Fraction {
+    private int numerateur;
+    private int denominateur;
     public String toString() {   return "Je suis une fraction.";}
+    public Fraction(int numerateur, int denominateur){
+        this.numerateur = numerateur;
+        this.denominateur = denominateur;
+    }
+    public Fraction(int numerateur){
+        this(numerateur,1);
+    }
+    public Fraction(){
+        this(0, 1);
+    }
+    public static final Fraction ZERO = new Fraction(0,1);
+    public static final Fraction UN = new Fraction(1,1);
+
+    public int getNumerateur(){return this.numerateur;}
+    public int getDenominateur(){return this.denominateur;}
+
+    public double doubleValue(){
+        return (double)this.numerateur / this.denominateur;
+    }
+
+    public Fraction add(Fraction f1, Fraction f2){
+        int num =(f1.numerateur * f2.denominateur) + (f1.denominateur * f2.numerateur);
+        int denum = (f1.denominateur * f2.denominateur);
+        return new Fraction(num, denum);
+    }
+
+    public boolean egal(Fraction f1, Fraction f2){
+        return (f1.numerateur/f1.denominateur) == (f2.numerateur/f2.denominateur);
+    }
+
+    public int compareTo(Fraction f1, Fraction f2  ) {
+        return Integer.compare(
+                f1.numerateur * f2.denominateur,
+                f2.numerateur * f1.denominateur
+        );
+    }
+
+
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,14 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Fraction f = new Fraction(); System.out.println(f);
+        Fraction f1 = new Fraction(3, 4);
+        Fraction f2 = new Fraction(6);
+        Fraction f3 = new Fraction();
+
+        assert f1.toString().equal(3,4);
+        assert f2.toString().equal(6);
+        assert f3.toString().equal();
+
 
 
     }
